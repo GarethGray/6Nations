d239e08029338c978824746d0c11b2563f0ef464
Gareth Gray <ggray10@qub.ac.uk>
2017-03-20 15:27:02 +0000

    Undone Kathy Testing comment in Application

---
+++ Programming Project/src/uk/ac/qub/SixNationsProject/League.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package uk.ac.qub.SixNationsProject;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author garethgray
+ *
+ */
+public class League {
+	
+	private Map<TeamName, Integer> pointsScored = new TreeMap<TeamName, Integer>();
+	private Map<TeamName, Integer> pointsConceded = new TreeMap<TeamName, Integer>();
+	private Map<TeamName, Integer> triesScored = new TreeMap<TeamName, Integer>();
+	private Map<TeamName, Integer> bonusPointsScored = new TreeMap<TeamName, Integer>();
+
+
+}
