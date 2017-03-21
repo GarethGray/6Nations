926faf9590647d29f339b75e6d263b91393305bb
Gareth Gray <ggray10@qub.ac.uk>
2017-03-21 12:50:14 +0000

    Removing Laura's testing comment

--- Programming Project/src/uk/ac/qub/SixNationsProject/Application.java
+++ Programming Project/src/uk/ac/qub/SixNationsProject/Application.java
@@ -16,7 +16,6 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Laura testing\n");
 		
 		Team scotland = new Team(TeamName.SCOTLAND);
 		Team france = new Team(TeamName.FRANCE);
