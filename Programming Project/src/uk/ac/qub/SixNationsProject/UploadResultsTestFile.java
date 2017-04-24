7382f056aa806d05ce4a2d792b3f387f3f317ec0
Laura McCormick <lmccormick07@qmail.ac.uk>
2017-04-24 11:12:55 +0100

    UploadResultsTestFile now features a method to update scores from a text file

--- Programming Project/src/uk/ac/qub/SixNationsProject/UploadResultsTestFile.java
+++ Programming Project/src/uk/ac/qub/SixNationsProject/UploadResultsTestFile.java
@@ -5,18 +5,77 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UploadResultsTestFile {
-	public static void main(String[] args) {
+	public static void uploadResults(Tournament tournament, String fileName) {
+		int roundNumber = 0;
+		int fixtureNumber = 0;
+		Fixture chosenFixture;
+		int team1Tries;
+		int team1Score;
+		int team2Tries;
+		int team2Score;
+		int info = 1;
+		
+		ArrayList<String> list = readFile(fileName);
+		
+		// gleans round number from second line of file
+		while (roundNumber < 1 || roundNumber > tournament.getRounds().size()) {
+			roundNumber = Integer.valueOf(list.get(info));
+		}
+		info++;
+		roundNumber--;
+
+		// prints fixtures of chosen round
+		Round chosenRound = tournament.getRounds().get(roundNumber);
+		chosenRound.printFixtures();
+		
+		int numberOfFixtures = chosenRound.getFixtures().size();
+		
+		// gleans fixture number from third line of file
+		while (fixtureNumber < 1 || fixtureNumber > numberOfFixtures) {
+			fixtureNumber = Integer.valueOf(list.get(info));
+		}
+		info++;
+		fixtureNumber--;
+		chosenFixture = chosenRound.getFixtures().get(fixtureNumber);
+
+		// gleans scores from fourth, fifth, sixth and seventh line of file
+		team1Tries = Integer.valueOf(list.get(info));
+		info++;
+		team1Score = Integer.valueOf(list.get(info));
+		info++;
+
+		team2Tries = Integer.valueOf(list.get(info));
+		info++;
+		team2Score = Integer.valueOf(list.get(info));
+		info++;
+
+		int[] team1ScoreArray = { team1Tries, team1Score };
+		int[] team2ScoreArray = { team2Tries, team2Score };
+
+		Result result = chosenFixture.getResult();
+		result.setScores(team1ScoreArray, team2ScoreArray);
+
+		// Update the teams with their points and scores
+		chosenFixture.updateTeamsValues();
+		chosenFixture.printFixtureResult();
+		
+		System.out.println("Done");
+	}
+	
+	public static ArrayList<String> readFile(String fileName) {
+		ArrayList<String> list = new ArrayList<String>();
+		
 		try {
-			File file = new File("TeamUpdate.txt");
+			File file = new File(fileName);
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 
 			String line = br.readLine();
-
 			while (line != null) {
-				System.out.println(line);
+				list.add(line);
 				line = br.readLine();
 			}
 
@@ -30,5 +89,6 @@ public class UploadResultsTestFile {
 			e.printStackTrace();
 		}
 
+		return list;
 	}
 }
