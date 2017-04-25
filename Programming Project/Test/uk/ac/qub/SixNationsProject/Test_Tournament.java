9ab993a71193963baa8588c602d9639e7f19d82a
Gareth Gray <ggray10@qub.ac.uk>
2017-04-25 14:41:37 +0100

    Test_Tournament, Test_Round

--- Programming Project/Test/uk/ac/qub/SixNationsProject/Test_Tournament.java
+++ Programming Project/Test/uk/ac/qub/SixNationsProject/Test_Tournament.java
@@ -28,10 +28,8 @@ public class Test_Tournament {
 		year = 2017;
 		fixtures = new ArrayList<Fixture>();
 		teams = new ArrayList<>();
-<<<<<<< HEAD
-		fg = new Tournament(teams, year);
-=======
->>>>>>> branch 'New-Unit-Testing' of https://github.com/GarethGray/6Nations.git
+		tournament = new Tournament(teams, year);
+
 
 		Team ireland = new Team(TeamName.IRELAND);
 		Team france = new Team(TeamName.FRANCE);
