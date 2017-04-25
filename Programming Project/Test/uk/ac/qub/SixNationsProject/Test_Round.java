d41d83c9ea47d7d402bc4b44b6c64b68acbcad86
Gareth Gray <ggray10@qub.ac.uk>
2017-04-25 14:37:50 +0100

    # WARNING: head commit changed in the meantime
    Minor changes

--- Programming Project/Test/uk/ac/qub/SixNationsProject/Test_Round.java
+++ Programming Project/Test/uk/ac/qub/SixNationsProject/Test_Round.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.junit.Before;
@@ -20,51 +21,48 @@ public class Test_Round {
 
 	// instantiation of bytearray stream to capture output from print method
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	
+
 	int validFixtureNumber, validRoundNumber;
 	Team team1, team2, team3, team4, team5, team6;
 	Fixture fixture1, fixture2, fixture3;
-	
-	
+
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		
+
 		validFixtureNumber = 1;
 		validRoundNumber = 1;
-		
-		
+
 		team1 = new Team(TeamName.ENGLAND);
 		team2 = new Team(TeamName.FRANCE);
 		team3 = new Team(TeamName.IRELAND);
 		team4 = new Team(TeamName.ITALY);
 		team5 = new Team(TeamName.SCOTLAND);
 		team6 = new Team(TeamName.WALES);
-		
+
 		fixture1 = new Fixture(validFixtureNumber, team1, team2);
 		fixture2 = new Fixture(validFixtureNumber, team3, team4);
 		fixture3 = new Fixture(validFixtureNumber, team5, team6);
-		
-		
+
 	}
 
 	/**
-	 * Test method for {@link uk.ac.qub.SixNationsProject.Round#Round(uk.ac.qub.SixNationsProject.Fixture, uk.ac.qub.SixNationsProject.Fixture, uk.ac.qub.SixNationsProject.Fixture, int)}.
+	 * Test method for
+	 * {@link uk.ac.qub.SixNationsProject.Round#Round(uk.ac.qub.SixNationsProject.Fixture, uk.ac.qub.SixNationsProject.Fixture, uk.ac.qub.SixNationsProject.Fixture, int)}.
 	 */
 	@Test
 	public void testRoundConstructor() {
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
 		assertTrue(round != null);
-		
+
 		assertEquals(fixture1, round.getFixture1());
 		assertEquals(fixture2, round.getFixture2());
 		assertEquals(fixture3, round.getFixture3());
 		assertEquals(validFixtureNumber, round.getNumber());
-		
-	
+
 	}
 
 	/**
@@ -72,12 +70,11 @@ public class Test_Round {
 	 */
 	@Test
 	public void testSetGetFixture1() {
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
 		round.setFixture1(fixture2);
 		assertEquals(fixture2, round.getFixture1());
-		
-		
+
 	}
 
 	/**
@@ -85,7 +82,7 @@ public class Test_Round {
 	 */
 	@Test
 	public void testSetGetFixture2() {
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
 		round.setFixture2(fixture1);
 		assertEquals(fixture1, round.getFixture2());
@@ -96,11 +93,11 @@ public class Test_Round {
 	 */
 	@Test
 	public void testGetFixture3() {
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
 		round.setFixture3(fixture1);
 		assertEquals(fixture1, round.getFixture3());
-	
+
 	}
 
 	/**
@@ -115,65 +112,109 @@ public class Test_Round {
 
 	/**
 	 * Test method for print fixtures
-	 * 
-	 * 
 	 */
 	@Test
 	public void testPrintFixturesNotVoid() {
-		
-		
+
 		System.setOut(new PrintStream(outContent));
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
-		
-		
+
 		round.printFixtures();
-		
-		//Test that the output is not null
+
+		// Test that the output is not null
 		assertTrue(outContent.toString() != null);
-		
-		//set up expected output
-		String expected = ("Round " + round.getNumber()+"\n"+"1. "+fixture1.getTeam1().getName()+" vs "+fixture1.getTeam2().getName()+"\n"+
-		"2. "+fixture2.getTeam1().getName()+" vs "+fixture2.getTeam2().getName()+"\n"
-		+"3. "+fixture3.getTeam1().getName()+" vs "+fixture3.getTeam2().getName()+"\n"+" \n");
-		  
-		 //comparison
+
+		// set up expected output
+		String expected = ("Round " + round.getNumber() + "\n" + "1. " + fixture1.getTeam1().getName() + " vs "
+				+ fixture1.getTeam2().getName() + "\n" + "2. " + fixture2.getTeam1().getName() + " vs "
+				+ fixture2.getTeam2().getName() + "\n" + "3. " + fixture3.getTeam1().getName() + " vs "
+				+ fixture3.getTeam2().getName() + "\n" + " \n");
+
+		// comparison
 		assertEquals(expected, outContent.toString());
-		
-		
+
 		System.setOut(null);
 
-		 
-	   
 	}
-	
+
+	/**
+	 * Testing the printFixtures method
+	 */
+
 	@Test
 	public void testPrintFixtures() {
-		
-		
+
 		System.setOut(new PrintStream(outContent));
-		
+
 		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
-		
-		
+
 		round.printFixtures();
-		
-		
-		//set up expected output
-		String expected = ("Round " + round.getNumber()+"\n"+"1. "+fixture1.getTeam1().getName()+" vs "+fixture1.getTeam2().getName()+"\n"+
-		"2. "+fixture2.getTeam1().getName()+" vs "+fixture2.getTeam2().getName()+"\n"
-		+"3. "+fixture3.getTeam1().getName()+" vs "+fixture3.getTeam2().getName()+"\n"+" \n");
-		  
-		 //comparison
+
+		// set up expected output
+		String expected = ("Round " + round.getNumber() + "\n" + "1. " + fixture1.getTeam1().getName() + " vs "
+				+ fixture1.getTeam2().getName() + "\n" + "2. " + fixture2.getTeam1().getName() + " vs "
+				+ fixture2.getTeam2().getName() + "\n" + "3. " + fixture3.getTeam1().getName() + " vs "
+				+ fixture3.getTeam2().getName() + "\n" + " \n");
+
+		// comparison
 		assertEquals(expected, outContent.toString());
-		
-		
+
 		System.setOut(null);
 
-		 
-	   
+	}
+
+	/**
+	 * Tests the setFixtures method in round to check the individual fixtures
+	 * are correctly added to the ArrayList fixtures
+	 */
+
+	@Test
+	public void testSetFixtures() {
+		ArrayList<Fixture> expectedFixtures = new ArrayList<>();
+		expectedFixtures.add(fixture1);
+		expectedFixtures.add(fixture2);
+		expectedFixtures.add(fixture3);
+
+		Round round = new Round(fixture1, fixture2, fixture3, 1);
+
+		round.setFixtures();
+
+		assertEquals(round.getFixtures().get(0), expectedFixtures.get(0));
+		assertEquals(round.getFixtures().get(1), expectedFixtures.get(1));
+		assertEquals(round.getFixtures().get(2), expectedFixtures.get(2));
+
 	}
 
 	
+	//TODO 
+	/**
+	 * This SHOULD work but for some reason does not. Fresh eyes appreciated.
+	 */
+	@Test
+	public void testPrintRoundResults() {
+
+		System.setOut(new PrintStream(outContent));
+
+		Round round = new Round(fixture1, fixture2, fixture3, validRoundNumber);
+
+		round.printRoundResults();
+
+		// set up expected output
+		String expected = ("Round: " + round.getNumber() + "\nFixture: 1\n" + "\t\t" + team1.getName() + "\t vs\t"
+				+ team2.getName() + "\nTRIES:\t\t" + 0 + "\t\t\t" + 0 + "\nSCORE:\t\t" + 0 + "\t\t\t" + 0
+				+ "\nPOINTS:\t\t" + 0 + "\t\t\t" + 0 + "\nBONUS:\t\t" + 0 + "\t\t\t" + 0 + "\n" + "\nFixture: 1\n"
+				+ "\t\t" + team3.getName() + "\t vs\t" + team4.getName() + "\nTRIES:\t\t" + 0 + "\t\t\t" + 0
+				+ "\nSCORE:\t\t" + 0 + "\t\t\t" + 0 + "\nPOINTS:\t\t" + 0 + "\t\t\t" + 0 + "\nBONUS:\t\t" + 0 + "\t\t\t"
+				+ 0 + " " + "\n\nFixture: 1\n" + "\t\t" + team5.getName() + "\t vs\t" + team6.getName() + "\nTRIES:\t\t"
+				+ 0 + "\t\t\t" + 0 + "\nSCORE:\t\t" + 0 + "\t\t\t" + 0 + "\nPOINTS:\t\t" + 0 + "\t\t\t" + 0
+				+ "\nBONUS:\t\t" + 0 + "\t\t\t" + 0+"\n\n");
+
+		// comparison
+		assertEquals(expected, outContent.toString());
+
+		System.setOut(null);
+
+	}
 
 }
---
+++ Programming Project/src/uk/ac/qub/SixNationsProject/Sandbox.java
@@ -0,0 +1,77 @@
+package uk.ac.qub.SixNationsProject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+public class Sandbox {
+	
+
+	public static void main(String[] args) {
+		ArrayList<Fixture> fixtures;
+		Tournament tournament;
+		int year;
+		ArrayList<Team> teams;
+		ArrayList<Round> rounds;
+		// instantiation of bytearray stream to capture output from print method
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		Fixture fixture1, fixture2, fixture3;
+		Result result1, result2, result3;
+		int[] team3score, team4score, team5score, team6score;
+		// TODO Auto-generated method stub
+		year = 2017;
+		fixtures = new ArrayList<Fixture>();
+		teams = new ArrayList<>();
+
+		Team ireland = new Team(TeamName.IRELAND);
+		Team france = new Team(TeamName.FRANCE);
+		Team england = new Team(TeamName.ENGLAND);
+		Team italy = new Team(TeamName.ITALY);
+		Team wales = new Team(TeamName.WALES);
+		Team scotland = new Team(TeamName.SCOTLAND);
+
+		teams.add(scotland);
+		teams.add(wales);
+		teams.add(italy);
+		teams.add(france);
+		teams.add(england);
+		teams.add(ireland);
+
+		tournament = new Tournament(teams, year);
+
+		rounds = tournament.generateRounds(year, teams);
+		for (Round round : rounds) {
+			fixtures.addAll(round.getFixtures());
+		}
+
+		fixture1 = new Fixture(1, ireland, france);
+		fixture2 = new Fixture(2, england, italy);
+		fixture3 = new Fixture(3, wales, scotland);
+
+		int[] team1score = { 22, 4 };
+		int[] team2score = { 0, 0 };
+
+		// team3score
+		// team4score
+		// team5score
+		// team6score;
+
+		result1 = new Result();
+		result1.setScores(team1score, team2score);
+		result2 = new Result();
+		result2.setScores(team1score, team2score);
+		result3 = new Result();
+		result3.setScores(team1score, team2score);
+
+		fixture1.setResult(result1);
+		fixture2.setResult(result2);
+		fixture3.setResult(result3);
+		
+		Round round = new Round(fixture1, fixture2, fixture3, 1);
+		
+		rounds.add(round);
+		
+		tournament.printTournamentResults();
+
+	}
+
+}
