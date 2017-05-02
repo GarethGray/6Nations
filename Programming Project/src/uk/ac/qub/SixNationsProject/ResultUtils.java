af9df1f5845b43a2553e9ccfd5c8b7e60cded4d0
Laura McCormick <lmccormick07@qmail.ac.uk>
2017-05-02 14:31:24 +0100

    ResultUtils.promptToInsertResults now inserts into database

--- Programming Project/src/uk/ac/qub/SixNationsProject/ResultUtils.java
+++ Programming Project/src/uk/ac/qub/SixNationsProject/ResultUtils.java
@@ -1,6 +1,7 @@
 package uk.ac.qub.SixNationsProject;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -30,73 +31,84 @@ public final class ResultUtils {
 		return fixture.getResult();
 	}
 
-// TODO connect to database and do some JDBC - Laura
-	public static void promptToInsertResults(Tournament tournament, Scanner scanner) {
-		int tournamentYear=0;
-		int roundNumber = 0;
-		int fixtureNumber = 0;
-		Fixture chosenFixture;
-		int team1Tries;
-		int team1Score;
-		int team2Tries;
-		int team2Score;
+	// TODO connect to database and do some JDBC - Laura
+	public static void promptToInsertResults(Scanner scanner) {
+		try {
+			Connection conn = DbConnect.getRemoteConnection();
+			int tournamentYear = 0;
+			int roundNumber = 0;
+			int numberOfRounds = 3;
+			int fixtureNumber = 0;
+			int teamHTries;
+			int teamHScore;
+			int teamATries;
+			int teamAScore;
+			String teamHome=null;
+			String teamAway=null;
 		
-		// asks user to select tournament
-		while (tournamentYear < 1) {
-			System.out.println("Select tournament year:");
-			tournamentYear = scanner.nextInt();
-		}
+			// asks user to select tournament
+			while (tournamentYear < 1) {
+				System.out.println("Select tournament year:");
+				tournamentYear = scanner.nextInt();
+			}
 		
 		// asks user to select round to input scores into
-		while (roundNumber < 1 || roundNumber > tournament.getRounds().size()) {
+		while (roundNumber < 1 || roundNumber > numberOfRounds) {
 			System.out.println("Select round:");
 			roundNumber = scanner.nextInt();
 		}
-		
 
-		// prints fixtures of chosen round
-		Round chosenRound = tournament.getRounds().get(roundNumber-1);
-		chosenRound.printFixtures();
-
-		int numberOfFixtures = chosenRound.getFixtures().size();
 		// asks user to select fixture to input scores into
-		while (fixtureNumber < 1 || fixtureNumber > numberOfFixtures) {
+		while (fixtureNumber < 1 || fixtureNumber > 5) {
 			System.out.println("Select fixture:");
 			fixtureNumber = scanner.nextInt();
 		}
-		chosenFixture = chosenRound.getFixtures().get(fixtureNumber-1);
 
+		// uses user-inputted values to select HOME team from database
+		Statement getTeams = conn.createStatement();
+		ResultSet rs = getTeams.executeQuery("SELECT TeamNameHome FROM Fixture WHERE Year = "+tournamentYear+" AND RoundNo = "+roundNumber+" AND FixtureNo = "+fixtureNumber+";");
+		while (rs.next()){
+			teamHome=rs.getString("TeamNameHome");
+		}
+		System.out.println("\nHome team: "+teamHome);
+		
+		//uses user-inputted values to select AWAY team from database
+		rs = getTeams.executeQuery("SELECT TeamNameAway FROM Fixture WHERE Year = "+tournamentYear+" AND RoundNo = "+roundNumber+" AND FixtureNo = "+fixtureNumber+";");
+		while (rs.next()){
+			teamAway=rs.getString("TeamNameAway");
+		}
+		System.out.println("\nHome team: "+teamAway);
+		
 		// asks user to input scores for each team
 		System.out.println("Please enter scores:");
-		System.out.println(chosenFixture.getTeam1().getName() + " tries:");
-		team1Tries = scanner.nextInt();
-		System.out.println(chosenFixture.getTeam1().getName() + " score:");
-		team1Score = scanner.nextInt();
+		System.out.println(teamHome + " tries:");
+		teamHTries = scanner.nextInt();
+		System.out.println(teamHome + " score:");
+		teamHScore = scanner.nextInt();
 
-		System.out.println(chosenFixture.getTeam2().getName() + " tries:");
-		team2Tries = scanner.nextInt();
-		System.out.println(chosenFixture.getTeam2().getName() + " score:");
-		team2Score = scanner.nextInt();
+		System.out.println(teamAway + " tries:");
+		teamATries = scanner.nextInt();
+		System.out.println(teamAway + " score:");
+		teamAScore = scanner.nextInt();
 
-		int[] team1ScoreArray = { team1Tries, team1Score };
-		int[] team2ScoreArray = { team2Tries, team2Score };
+		//int[] team1ScoreArray = { teamHTries, teamHScore };
+		//int[] team2ScoreArray = { teamATries, teamAScore };
 
-		Result result = chosenFixture.getResult();
-		result.setScores(team1ScoreArray, team2ScoreArray);
+		//Result result = chosenFixture.getResult();
+		//result.setScores(team1ScoreArray, team2ScoreArray);
 
 		// Update the teams with their points and scores
-		chosenFixture.updateTeamsValues();
-		chosenFixture.printFixtureResult();
+		//chosenFixture.updateTeamsValues();
+		//chosenFixture.printFixtureResult();
 		
 		// the code inside the try block inserts results for each team in the fixture specified
-		try {
-			Connection conn = DbConnect.getRemoteConnection();
+
 			Statement insertResult = conn.createStatement();
 			
-			String newResultTeamHome = "INSERT INTO FixtureResult Values("+tournament.getYear()+", '"+ tournament.getYear() + "" + roundNumber + "" + fixtureNumber + "', '"+chosenFixture.getTeam1().getName()+"'"
-					+ ", "+team1Tries+", "+team1Score+");";
-			String newResultTeamAway = "INSERT INTO FixtureResult Values("+tournament.getYear()+", '"+ tournament.getYear() + "" + roundNumber + "" + fixtureNumber + "', '"+chosenFixture.getTeam2().getName()+"'"
-					+ ", "+team2Tries+", "+team2Score+");";
+			String newResultTeamHome = "INSERT INTO FixtureResult Values("+tournamentYear+", '"+ tournamentYear + "" + roundNumber + "" + fixtureNumber + "', '"+teamHome+"'"
+					+ ", "+teamHTries+", "+teamHScore+");";
+			String newResultTeamAway = "INSERT INTO FixtureResult Values("+tournamentYear+", '"+ tournamentYear + "" + roundNumber + "" + fixtureNumber + "', '"+teamAway+"'"
+					+ ", "+teamATries+", "+teamAScore+");";
 			
 			insertResult.addBatch(newResultTeamHome);
 			insertResult.addBatch(newResultTeamAway);
