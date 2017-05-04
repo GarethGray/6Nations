b4dcb9d6b150a4fbb84de22d20518e225131b020
Laura McCormick <lmccormick07@qmail.ac.uk>
2017-05-04 10:14:15 +0100

    splitting promptToInsertResults into smaller methods

--- Programming Project/src/uk/ac/qub/SixNationsProject/ResultUtils.java
+++ Programming Project/src/uk/ac/qub/SixNationsProject/ResultUtils.java
@@ -57,42 +57,24 @@ public final class ResultUtils {
 			String teamAway = null;
 
 			// asks user to select tournament
-			while (tournamentYear < 1) {
-				System.out.println("Select tournament year:");
-				tournamentYear = scanner.nextInt();
-			}
+			tournamentYear = promptForTournamentYear(scanner, tournamentYear);
 
 			// asks user to select round to input scores into
-			while (roundNumber < 1 || roundNumber > numberOfRounds) {
-				System.out.println("Select round:");
-				roundNumber = scanner.nextInt();
-			}
+			roundNumber = promptForRoundNumber(scanner, roundNumber, numberOfRounds);
 
 			// asks user to select fixture to input scores into
-			while (fixtureNumber < 1 || fixtureNumber > 5) {
-				System.out.println("Select fixture:");
-				fixtureNumber = scanner.nextInt();
-			}
+			fixtureNumber = promptForFixtureNumber(scanner, fixtureNumber);
 			
-			// uses user-inputed values to select HOME team from database
+			// uses user-inputed values to select HOME team from database, prints "Home team: TEAM"
 			Statement getTeams = conn.createStatement();
-			ResultSet rs = getTeams.executeQuery("SELECT TeamNameHome FROM Fixture WHERE Year = " + tournamentYear
-					+ " AND RoundNo = " + roundNumber + " AND FixtureNo = " + fixtureNumber + ";");
-			while (rs.next()) {
-				teamHome = rs.getString("TeamNameHome");
-			}
-			System.out.println("\nHome team: " + teamHome);
+			teamHome = getHomeTeamName(tournamentYear, roundNumber, fixtureNumber, teamHome, getTeams);
 
-			// uses user-inputed values to select AWAY team from database
-			rs = getTeams.executeQuery("SELECT TeamNameAway FROM Fixture WHERE Year = " + tournamentYear
-					+ " AND RoundNo = " + roundNumber + " AND FixtureNo = " + fixtureNumber + ";");
-			while (rs.next()) {
-				teamAway = rs.getString("TeamNameAway");
-			}
-			System.out.println("\nAway team: " + teamAway);
+			// uses user-inputed values to select AWAY team from database, prints "Away team: TEAM"
+			teamAway = getAwayTeamName(tournamentYear, roundNumber, fixtureNumber, teamAway, getTeams);
+			
 
 			// asks user to input scores for each team
-			System.out.println("Please enter scores:");
+			System.out.println("\n\nPlease enter scores:");
 			System.out.println(teamHome + " tries:");
 			teamHTries = scanner.nextInt();
 			System.out.println(teamHome + " score:");
@@ -102,33 +84,29 @@ public final class ResultUtils {
 			teamATries = scanner.nextInt();
 			System.out.println(teamAway + " score:");
 			teamAScore = scanner.nextInt();
-
-			// int[] team1ScoreArray = { teamHTries, teamHScore };
-			// int[] team2ScoreArray = { teamATries, teamAScore };
-
-			// Result result = chosenFixture.getResult();
-			// result.setScores(team1ScoreArray, team2ScoreArray);
-
-			// Update the teams with their points and scores
-			// chosenFixture.updateTeamsValues();
-			// chosenFixture.printFixtureResult();
-
 			
 			// checks the values the user has entered
 			if(validateScore(teamHTries, teamHScore) && validateScore(teamATries, teamAScore)) {
 			
-			Statement insertResult = conn.createStatement();
-
+			Team home = new Team(TeamName.valueOf(teamHome));
+			Team away = new Team(TeamName.valueOf(teamAway));
+			home.setTries(teamHTries);
+			home.setScoreFor(teamHScore);
+			away.setTries(teamATries);
+			away.setScoreFor(teamAScore);
+			
 			// creates statement to insert HOME team results into FixtureResult
 			String newResultTeamHome = "INSERT INTO FixtureResult Values(" + tournamentYear + ", '" + tournamentYear
 					+ "" + roundNumber + "" + fixtureNumber + "', '" + teamHome + "'" + ", " + teamHTries + ", "
 					+ teamHScore + ");";
+			
 			// creates statement to insert AWAY team results into FixtureResult
 			String newResultTeamAway = "INSERT INTO FixtureResult Values(" + tournamentYear + ", '" + tournamentYear
 					+ "" + roundNumber + "" + fixtureNumber + "', '" + teamAway + "'" + ", " + teamATries + ", "
 					+ teamAScore + ");";
 
 			// adds statements to insertResult batch and executes them
+			Statement insertResult = conn.createStatement();
 			insertResult.addBatch(newResultTeamHome);
 			insertResult.addBatch(newResultTeamAway);
 			insertResult.executeBatch();
@@ -144,6 +122,117 @@ public final class ResultUtils {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+	
+	
+	/**
+	 * Reads in year, round and fixture and returns the AWAY team name as a String
+	 * @param tournamentYear
+	 * @param roundNumber
+	 * @param fixtureNumber
+	 * @param teamAway
+	 * @param getTeams
+	 * @return
+	 * @throws SQLException
+	 */
+	public static String getAwayTeamName(int tournamentYear, int roundNumber, int fixtureNumber, String teamAway,
+			Statement getTeams) throws SQLException {
+		ResultSet rs = getTeams.executeQuery("SELECT TeamNameAway FROM Fixture WHERE Year = " + tournamentYear
+				+ " AND RoundNo = " + roundNumber + " AND FixtureNo = " + fixtureNumber + ";");
+		while (rs.next()) {
+			teamAway = rs.getString("TeamNameAway");
+		}
+		System.out.println("\nAway team: " + teamAway);
+		return teamAway;
+	}
+
+	
+	
+
+	
+	/**
+	 * Reads in year, round and fixture and returns the HOME team name as a String
+	 * @param tournamentYear
+	 * @param roundNumber
+	 * @param fixtureNumber
+	 * @param teamHome
+	 * @param getTeams
+	 * @return
+	 * @throws SQLException
+	 */
+	public static String getHomeTeamName(int tournamentYear, int roundNumber, int fixtureNumber, String teamHome,
+			Statement getTeams) throws SQLException {
+		ResultSet rs = getTeams.executeQuery("SELECT TeamNameHome FROM Fixture WHERE Year = " + tournamentYear
+				+ " AND RoundNo = " + roundNumber + " AND FixtureNo = " + fixtureNumber + ";");
+		while (rs.next()) {
+			teamHome = rs.getString("TeamNameHome");
+		}
+		System.out.println("\nHome team: " + teamHome);
+		return teamHome;
+	}
+
+	
+	
+
+	
+	/**
+	 * Prompts user for a fixture number and returns it as an integer
+	 * @param scanner
+	 * @param fixtureNumber
+	 * @return
+	 */
+	public static int promptForFixtureNumber(Scanner scanner, int fixtureNumber) {
+		while (fixtureNumber < 1 || fixtureNumber > 5) {
+			System.out.println("Select fixture:");
+			fixtureNumber = scanner.nextInt();
+		}
+		return fixtureNumber;
+	}
+
+	
+	
+
+	
+	/**
+	 * Prompts user for a round number and returns it as an integer
+	 * @param scanner
+	 * @param roundNumber
+	 * @param numberOfRounds
+	 * @return
+	 */
+	public static int promptForRoundNumber(Scanner scanner, int roundNumber, int numberOfRounds) {
+		while (roundNumber < 1 || roundNumber > numberOfRounds) {
+			System.out.println("Select round:");
+			roundNumber = scanner.nextInt();
+		}
+		return roundNumber;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Prompts user for a year and returns it as an integer
+	 * @param scanner
+	 * @param tournamentYear
+	 * @return
+	 */
+	public static int promptForTournamentYear(Scanner scanner, int tournamentYear) {
+		while (tournamentYear < 1) {
+			System.out.println("Select tournament year:");
+			tournamentYear = scanner.nextInt();
+		}
+		return tournamentYear;
+	}
+
+	
+	
+	
+
+	
 
 	/**
 	 * This method asks users for the year, round and fixture that they wish to view results for
