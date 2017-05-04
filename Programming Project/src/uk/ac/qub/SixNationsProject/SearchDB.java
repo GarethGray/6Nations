5c9d2f12cd03609d6fbf8e907d18499ee75e51da
9261257 <9261257@ecs2-3402.eeecs.qub.ac.uk>
2017-05-04 17:36:16 +0100

    It appears to be a working menu of search options(!!!)

--- Programming Project/src/uk/ac/qub/SixNationsProject/SearchDB.java
+++ Programming Project/src/uk/ac/qub/SixNationsProject/SearchDB.java
@@ -18,50 +18,119 @@ public class SearchDB {
 	 */
 
 	public static void searchDatabase() {
-		// sets up scanner and assigns scanner input to variable
+
+		// sets up scanner and variables
 		Scanner scanner = new Scanner(System.in);
-		int option;
-		String searchTerm;
+		int option = 0;
 
-		// sets up menu for user
-		System.out.println("\n You can search the files in a number of ways. \n");
-		System.out.println("You can search by year or team name.\n");
-		System.out.println("If you would like to search by year press 1. If by team name press 2");
-		option = scanner.nextInt();
+		// menu continues unless exit is selected
+		while (option != 5) {
+			// declare var
+			String searchTerm;
 
-		try (Connection conn = DbConnect.getRemoteConnection();) {
+			// sets up menu for user
+			System.out.println("\nYou can search the files in a number of ways. \n");
+			System.out.println("If you would like to search by year enter 1 \nIf by team name enter 2");
+			System.out.println("If you would like to find the maximum score achieved in a year, enter 3");
+			System.out.println("If you would like to find the most tries scored in a year, enter 4");
+			System.out.println("If you would like to return to the main method, enter 5");
 
-			if (option == 1) {
-				System.out.println("What year would you like to see?");
-				searchTerm = scanner.nextLine();
+			// Takes user input
+			option = scanner.nextInt();
 
-				// creates a sql statement and uses user input of year
-				Statement getSearch = conn.createStatement();
-				ResultSet rs = getSearch.executeQuery(
-						"SELECT TeamName, Tries, Score FROM FixtureResult WHERE Year = '" + searchTerm + "';");
-				while (rs.next()) {
-					// prints out search result
-					System.out.println(rs.getString("TeamName") + "Year: " + rs.getString("Year") + " Tries: "
-							+ rs.getString("Tries") + ", Score: " + rs.getString("Score"));
+			try (Connection conn = DbConnect.getRemoteConnection();) {
+
+				switch (option) {
+
+				// user wishes to search by year
+				case 1:
+					System.out.println("What year would you like to see?");
+					searchTerm = String.valueOf(scanner.nextInt());
+
+					// creates a sql statement and uses user input of year
+					Statement getSearch = conn.createStatement();
+
+					ResultSet rs = getSearch.executeQuery(
+							"SELECT TeamName, Tries, Score FROM FixtureResult WHERE Year = " + searchTerm + ";");
+					while (rs.next()) {
+						System.out.println(rs.getString("TeamName") + " Tries: " + rs.getString("Tries") + ", Score: "
+								+ rs.getString("Score"));
+					}
+					break;
+
+				case 2:
+					// user searches by team name
+
+					System.out.println("What team do you want to search for?");
+					searchTerm = scanner.next();
+					searchTerm.toUpperCase();
+
+					// creates a sql state and uses user input of team name
+					Statement getSearch2 = conn.createStatement();
+					ResultSet rs2 = getSearch2
+							.executeQuery("SELECT TeamName, Year, Tries, Score FROM FixtureResult WHERE TeamName = '"
+									+ searchTerm + "';");
+					while (rs2.next()) {
+						// prints out search result
+						System.out.println(rs2.getString("TeamName") + " Year: " + rs2.getString("Year") + " Tries: "
+								+ rs2.getString("Tries") + ", Score: " + rs2.getString("Score"));
+					}
+
+					break;
+
+				case 3:
+					// user searches for the highest score
+
+					System.out.println("What year would you like to see the highest score achieved?");
+					searchTerm = String.valueOf(scanner.nextInt());
+
+					// creates a sql query and uses user input of year
+					Statement getSearch3 = conn.createStatement();
+					ResultSet rs3 = getSearch3.executeQuery(
+							"SELECT TeamName, MAX(PointsScored) From League WHERE Year =" + searchTerm + ";");
+					while (rs3.next()) {
+						// prints out search result
+						System.out
+								.println(rs3.getString("TeamName") + " Score:  " + rs3.getString("MAX(PointsScored)"));
+					}
+					break;
+
+				case 4:
+					// user searches for most tries
+					System.out.println("What year would you like to see the most tries achieved?");
+					searchTerm = String.valueOf(scanner.nextInt());
+
+					// creates a sql query and uses user input of year
+					Statement getSearch4 = conn.createStatement();
+					ResultSet rs4 = getSearch4
+							.executeQuery("SELECT TeamName, MAX(Tries) From League WHERE Year =" + searchTerm + ";");
+					while (rs4.next()) {
+						// prints out search result
+						System.out.println(rs4.getString("TeamName") + " Tries:  " + rs4.getString("MAX(Tries)"));
+					}
+					break;
+
+				case 5:
+					// user exists
+					System.out.println("Thank you for using the Six Nations search function. ");
+
+					break;
+
+				default:
+					// validation for bad user input
+					System.out.println("You have entered an invalid value. Please enter try again.");
+					break;
 				}
-			} else {
-				System.out.println("What team do you want to search for?");
-				searchTerm = scanner.nextLine();
-				Statement getSearch = conn.createStatement();
-				ResultSet rs = getSearch.executeQuery(
-						"SELECT TeamName, Tries, Score FROM FixtureResult WHERE TeamName = '" + searchTerm + "';");
-				while (rs.next()) {
-					// prints out search result
-					System.out.println(rs.getString("TeamName") + "Year: " + rs.getString("Year") + " Tries: "
-							+ rs.getString("Tries") + ", Score: " + rs.getString("Score"));
 
-				}
-
+				// connection closed
 				conn.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
+		// scanner closed
+		scanner.close();
 	}
 }
